2e69e604c1e3da6afc36ed00405a07b4ea172d89
Robert Snyder <r.snyder49@gmail.com>
2018-12-26 23:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function produceDrivingRange(trip) {
-  return function {
-    
-  }
+  return function(beginning, ending) {
+    let start = parseInt(beginning);
+    let end = parseInt(ending);
+    let diffTravel = Math.abs(end-start);
+    let difference = blockRange - diffTravel;
+    if(difference > 0) {
+      return `within range by ${difference}`;
+    }
+    else{
+      return `${Math.abs(difference)} blocks out of range`;
+    }
+  };
 }
